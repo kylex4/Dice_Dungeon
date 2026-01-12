91ce8c15b1e18ebc12e582ef31a94f917a89b24f
1N3 Computer <Sample@BTHS.gmail.com>
2026-01-12 11:05:02 -0500

    Initiate

--- src/Dice.java
+++ src/Dice.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Dice {
+    private int value1;
+    private int value2;
+    private int dieNumber;
+    private int reroll;
+
+    public Dice() {
+        this.value1 = 0;
+        this.value2 = 0;
+        this.reroll = 2;
+        this.dieNumber = 1;
+    }
+
+    Scanner scan = new Scanner(System.in);
+
+    public int getReroll() {
+        return reroll;
+    }
+
+    public int getNumber() {
+        return dieNumber;
+    }
+
+    public void normalDice(){
+        dieNumber = (int) (Math.random() * 6) + 1;
+
+
+
+    }
+
+    public int reroll(){
+        if (reroll > 0){
+            System.out.println("Would you like to reroll your die?\n[1]. Reroll (Current Rerolls: " + getReroll() + "\n[2]. Keep Current Die Value (" + getNumber() + ")");
+            int option = scan.nextInt();
+
+        }
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Player {
+    private String name;
+    private int health;
+    private int defense;
+    private int stagger;
+    private int level;
+    private int pips;
+
+
+    public Player(String name){
+        this.name = name;
+        this.health = 30;
+        this.defense = 1;
+        this.stagger = 15;
+        this.level = 1;
+        this.pips = 0;
+        this.reroll = 2;
+    }
+
+
+
+    public int roll(Dice dice_type){
+
+    }
 }
