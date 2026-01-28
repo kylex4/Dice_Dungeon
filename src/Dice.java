9fd0ac95b1d5980b5e7741b5cb3ae74dcc8b21a4
1N3 Computer <Sample@BTHS.gmail.com>
2026-01-28 11:04:15 -0500

    Initiate

--- src/Dice.java
+++ src/Dice.java
@@ -1,11 +1,26 @@
 import java.util.Scanner;
 
+
+/**
+ * This class represents a Dice class
+ * @author Kyle Xu
+ */
 public class Dice {
+    /** The number from the first die */
     private int value1;
+
+    /** The number from the second die */
+
     private int value2;
+
+    /** The number from the current die you are rolling */
     private int dieNumber;
+
+    /** The number of rerolls the user has */
     private int reroll;
 
+
+    /** Initiates the Dice class */
     public Dice() {
         this.value1 = 0;
         this.value2 = 0;
@@ -15,23 +30,49 @@ public class Dice {
 
     Scanner scan = new Scanner(System.in);
 
+    /** Returns the value on the first die
+     *
+     * @return The value of the first die
+     */
+
     public int getValue1() {
         return value1;
     }
+
+    /** Returns the value of the second die
+     *
+     * @return The value of the second die
+     */
 
     public int getValue2() {
         return value2;
     }
 
+    /** Returns the number of rerolls the user has
+     *
+     * @return The number of rerolls
+     */
+
     public int getReroll() {
         return reroll;
     }
+
+    /** Returns the current die value
+     *
+     * @return The current die value
+     */
 
     public int getNumber() {
         return dieNumber;
     }
 
-    // Earn a reroll every third turn, prevents obtaining additional reroll by going back and forth between actions
+    /** Checks if three turns has passed before giving the user a reroll without repeating on the same turn it is gained.
+     * Announces to the user that they'll gain a reroll
+     *
+     * @param turn The current turn the user is on
+     * @param name The name of the user
+     * @param gained Whether a reroll was already gained on the current turn
+     */
     public void gainReroll(int turn, String name, boolean gained){
         if (turn % 3 == 0 && !gained){
             System.out.println(name + " gains a reroll!");
@@ -45,9 +86,16 @@ public class Dice {
         }
     }
 
-    public int roll(String character){
+    /** Chooses a random number between 1 - 6 and sets it as the current die value.
+     * Announces the value of the current die the user has rolled as well
+     *
+     * @param name The name of the user
+     * @return The value of the latest die the user has rolled
+     */
+
+    public int roll(String name){
         dieNumber = (int) (Math.random() * 6) + 1;
-        System.out.println( character + " rolls a " + dieNumber);
+        System.out.println( name + " rolls a " + dieNumber);
         return dieNumber;
     }
 
